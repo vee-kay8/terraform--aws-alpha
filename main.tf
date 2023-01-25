d009511a1b7b95a83a1ec3935466595186a60785
Voke <vokeogigbah@gmail.com>
2023-01-25 17:00:37 +0100

    Configure2

--- main.tf
+++ main.tf
@@ -1,24 +1,90 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+locals {
+  vpc_cidr = "10.0.0.0/16"
 
-  required_version = ">= 1.2.0"
+  public_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
+
+  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
+
+  availability_zones = ["us-east-1a", "us-east-1b"]
 }
 
-provider "aws" {
-  region = "us-west-2"
+resource "aws_vpc" "main" {
+  cidr_block = local.vpc_cidr
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.public_cidr[count.index]
+
+  availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "public${count.index+1}"
   }
 }
 
+resource "aws_subnet" "private" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.private_cidr[count.index]
+
+  availability_zone = local.availability_zones[count.index]
+
+  tags = {
+    Name = "private${count.index+1}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  count = 2
+
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  count = 2
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+}
+
+resource "aws_route_table" "private" {
+  count = 2
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
