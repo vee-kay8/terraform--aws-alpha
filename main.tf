1782c47394890693ef1839d59f5c809c377194c5
Voke <vokeogigbah@gmail.com>
2023-01-25 14:47:39 +0100

    Configure main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,45 @@
+locals {
+  vpc_cidr = "10.0.0.0/16"
+
+  public_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
+
+  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
+
+  availability_zones = ["us-east-1a", "us-east-1b"]
+}
+
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block = local.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.public_cidr[count.index]
+
+  availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "main"
+    Name = "public${count.index+1}"
   }
 }
 
-resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "private" {
+  count = 2
 
-  availability_zone = "us-west-2a"
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.private_cidr[count.index]
+
+  availability_zone = local.availability_zones[count.index]
 
   tags = {
-    Name = "public1"
-  }
-}
-
-resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  availability_zone = "us-west-2b"
-
-  tags = {
-    Name = "public2"
-  }
-}
-
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "private1"
-  }
-}
-
-resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
-
-  availability_zone = "us-west-2b"
-
-  tags = {
-    Name = "private2"
+    Name = "private${count.index+1}"
   }
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
 }
 
 resource "aws_route_table" "public" {
@@ -66,98 +49,42 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-
-  tags = {
-    Name = "main"
-  }
 }
 
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public.id
+resource "aws_eip" "nat" {
+  count = 2
+
+  vpc = true
 }
 
-resource "aws_eip" "nat1" {
-  vpc      = true
+resource "aws_nat_gateway" "nat" {
+  count = 2
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 }
 
-resource "aws_eip" "nat2" {
-  vpc      = true
-}
+resource "aws_route_table" "private" {
+  count = 2
 
-resource "aws_nat_gateway" "nat1" {
-  allocation_id = aws_eip.nat1.id
-  subnet_id     = aws_subnet.public1.id
-}
-
-resource "aws_nat_gateway" "nat2" {
-  allocation_id = aws_eip.nat2.id
-  subnet_id     = aws_subnet.public2.id
-}
-
-resource "aws_route_table" "private1" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat1.id
-  }
-
-  tags = {
-    Name = "main"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 }
 
-resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private1.id
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
-
-
-resource "aws_route_table" "private2" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat2.id
-  }
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.private2.id
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
